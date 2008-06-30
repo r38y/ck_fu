150f56a6d9af9b3f87170654e45b314d1a47495e
Randy Schmidt <randy@umlatte.com>
2008-06-29 23:31:44 -0400

    Added an option to set the separator

--- lib/ck_fu.rb
+++ lib/ck_fu.rb
@@ -1,13 +1,14 @@
 module Umlatte
   module CkFu
     def ck_fu(options={})
+      separator = options[:separator] || "|"
       return "" if RAILS_ENV == 'production'
       xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
       xhtml.div :id => 'ck_fu', :class => RAILS_ENV do
         xhtml.text! "Env: #{RAILS_ENV.titlecase}"
-        xhtml.text! " | Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
-        xhtml.text! " | Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
-        xhtml.text! " | Revision: #{deployed_revision}" unless deployed_revision.blank?
+        xhtml.text! " #{separator} Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
+        xhtml.text! " #{separator} Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
+        xhtml.text! " #{separator} Revision: #{deployed_revision}" unless deployed_revision.blank?
       end
       return out
     end
