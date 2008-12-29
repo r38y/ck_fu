b97aae81d5ba896d5b0c9e7c48d895f09ca19133
Randy Schmidt <randy@umlatte.com>
2008-12-29 12:36:50 -0500

    Add ability to have links

--- lib/ck_fu.rb
+++ lib/ck_fu.rb
@@ -1,18 +1,22 @@
 module Umlatte
   module CkFu
     def ck_fu(options={})
-      separator = options[:separator] || "--"
+      separator = options[:separator] || "&sect;"
       do_not_show = options[:if] == false || RAILS_ENV == 'production'
+      links = options[:links] || []
       return "" if do_not_show
-      xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
-      xhtml.div :id => 'ck_fu', :class => RAILS_ENV do
-        xhtml.text! "Env: #{RAILS_ENV.titlecase}"
-        xhtml.text! " #{separator} Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
-        xhtml.text! " #{separator} Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
-        xhtml.text! " #{separator} Revision: #{deployed_revision}" unless deployed_revision.blank?
-        xhtml.text! " #{separator} Deployed: #{deployed_date}" unless deployed_date.blank?
+      # xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
+      content_tag :div, :id => 'ck_fu', :class => RAILS_ENV do
+        text = "Env: #{RAILS_ENV.titlecase}"
+        text += " #{separator} Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
+        text += " #{separator} Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
+        text += " #{separator} Revision: #{deployed_revision}" unless deployed_revision.blank?
+        text += " #{separator} Deployed: #{deployed_date}" unless deployed_date.blank?
+        options[:links].each do |link|
+          text += " #{separator} #{link_to link[0], link[1]}"
+        end
+        text
       end
-      return out
     end
     
     def deployed_revision
