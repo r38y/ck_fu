a2aae093bd35a2fd59c1ea9b174ca17b2282da0f
Randy Schmidt <randy@umlatte.com>
2008-12-29 13:37:10 -0500

    Make revision and date an option

--- lib/ck_fu.rb
+++ lib/ck_fu.rb
@@ -5,13 +5,12 @@ module Umlatte
       do_not_show = !options[:if] || RAILS_ENV == 'production'
       links = options[:links] || []
       return "" if do_not_show
-      # xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
       content_tag :div, :id => 'ck_fu', :class => RAILS_ENV do
         text = "Env: #{RAILS_ENV.titlecase}"
         text += " #{separator} Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
         text += " #{separator} Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
-        text += " #{separator} Revision: #{deployed_revision}" unless deployed_revision.blank?
-        text += " #{separator} Deployed: #{deployed_date}" unless deployed_date.blank?
+        text += " #{separator} Revision: #{deployed_revision}" if options[:revision] && !deployed_revision.blank?
+        text += " #{separator} Deployed: #{deployed_date}" if options[:date] && !deployed_date.blank?
         options[:links].each do |link|
           text += " #{separator} #{link_to link[0], link[1]}"
         end
