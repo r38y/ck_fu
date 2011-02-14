0f98f290583ad91a80efff030cf2a6545bb5e5f7
Randy Schmidt <randy@forge38.com>
2011-02-14 16:37:23 -0500

    Get rid of deprecation warnings

--- lib/ck_fu.rb
+++ lib/ck_fu.rb
@@ -1,11 +1,11 @@
 module Forge38
   def ck_fu(options={})
-    return "" if (options.has_key?(:if) ? !options[:if] : Rails.production?)
+    return "" if (options.has_key?(:if) ? !options[:if] : Rails.env.production?)
     separator = html_safe(options[:separator] || "&sect;")
-    content_tag :div, :id => 'ck_fu', :class => Rails.environment do
-      text = "Env: #{Rails.environment.titlecase}"
+    content_tag :div, :id => 'ck_fu', :class => Rails.env do
+      text = "Env: #{Rails.env.titlecase}"
       text += " #{separator} Current DB: #{ActiveRecord::Base.connection.current_database}" if ActiveRecord::Base.connection.respond_to?(:current_database)
-      text += " #{separator} Current DB: #{ActiveRecord::Base::configurations[RAILS_ENV]['dbfile']}" if ActiveRecord::Base::configurations[RAILS_ENV]['adapter'] == 'sqlite3'
+      text += " #{separator} Current DB: #{ActiveRecord::Base::configurations[Rails.env]['dbfile']}" if ActiveRecord::Base::configurations[Rails.env]['adapter'] == 'sqlite3'
       text += " #{separator} Revision: #{deployed_revision}" if deployed_revision.present? && (options[:revision].nil? || options[:revision])
       text += " #{separator} Deployed: #{deployed_date}" if deployed_date.present? && (options[:date].nil? || options[:date])
       (options[:links] || []).each do |link|
@@ -16,26 +16,16 @@ module Forge38
   end
 
   def deployed_revision
-    file = RAILS_ROOT + '/REVISION'
+    file = Rails.root + '/REVISION'
     File.exists?(file) ? File.open(file).read.strip : nil
   end
 
   def deployed_date
-    file = RAILS_ROOT + '/DATE'
+    file = Rails.root + '/DATE'
     File.exists?(file) ? File.open(file).read.strip : nil
   end
 
   def html_safe(string)
     string.respond_to?(:html_safe) ? string.html_safe : string
-  end
-end
-
-module Rails
-  def self.environment
-    ENV['RAILS_ENV'].to_s.downcase
-  end
-
-  def self.production?
-    environment == 'production'
   end
 end
